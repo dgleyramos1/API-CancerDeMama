6db0169499c710d66f3f14a30e1985f00df92996
Dgley Ramos <dgleysilva@gmail.com>
2022-03-10 01:58:44 -0300

    Criando controller para busca de faixa etaria

--- src/main/java/com/diodevweek/devweek/Controller/ControllerIncidencia.java
+++ src/main/java/com/diodevweek/devweek/Controller/ControllerIncidencia.java
@@ -6,10 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api")
 public class ControllerIncidencia {
     private final IncidenciaRepo ocRepository;
 
