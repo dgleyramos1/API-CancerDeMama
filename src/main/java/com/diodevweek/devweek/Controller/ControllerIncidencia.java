156f04f94bb1782817daf7113d231d8fda50df20
Dgley Ramos <dgleysilva@gmail.com>
2022-03-10 01:55:37 -0300

    Criando controller para busca de incidencias

---
+++ src/main/java/com/diodevweek/devweek/Controller/ControllerIncidencia.java
@@ -0,0 +1,37 @@
+package com.diodevweek.devweek.Controller;
+
+import com.diodevweek.devweek.Entity.IncidenciaExame;
+import com.diodevweek.devweek.Repository.IncidenciaRepo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ControllerIncidencia {
+    private final IncidenciaRepo ocRepository;
+
+    public ControllerIncidencia(IncidenciaRepo ocRepository) {
+        this.ocRepository = ocRepository;
+    }
+
+    @GetMapping("/ocorrencias")
+    public ResponseEntity<List<IncidenciaExame>> findOcorrencias(){
+        List<IncidenciaExame> listaOcorrencia = ocRepository.findAll();
+        if (listaOcorrencia.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<>(listaOcorrencia, HttpStatus.OK);
+    }
+
+    @GetMapping("/ocorrencia/{id}")
+    public ResponseEntity<IncidenciaExame> findOcorrenciasById(@PathVariable Long id){
+        Optional<IncidenciaExame> ocorrenciaOptional = ocRepository.findById(id);
+        if (ocorrenciaOptional.isPresent()){
+            IncidenciaExame ocorrenciaUnid = ocorrenciaOptional.get();
+            return new ResponseEntity<>(ocorrenciaUnid, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
