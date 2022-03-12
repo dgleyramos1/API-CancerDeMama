57cb37ec19ab38d03b8d6e4bc6869d923a0e0863
Dgley Ramos <dgleysilva@gmail.com>
2022-03-11 23:45:27 -0300

    criando metodo POST para regioes

--- src/main/java/com/diodevweek/devweek/Controller/ControllerRegiao.java
+++ src/main/java/com/diodevweek/devweek/Controller/ControllerRegiao.java
@@ -1,13 +1,11 @@
 package com.diodevweek.devweek.Controller;
 
+import com.diodevweek.devweek.Entity.FaixaEtaria;
 import com.diodevweek.devweek.Entity.Regiao;
 import com.diodevweek.devweek.Repository.RegiaoRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,5 +41,10 @@ public class ControllerRegiao {
             return new ResponseEntity<>(regiaoUnid, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/regioes")
+    public Regiao newRegiao(@RequestBody Regiao newRegiao){
+        return repository.save(newRegiao);
     }
 }
